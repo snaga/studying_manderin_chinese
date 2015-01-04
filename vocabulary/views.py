e85020ed2bee4ca822365c1c30b49fcb9a21d9b8
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-01-04 17:54:12 +0900

    Add pinyin lookup feature.

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,6 +1,35 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from studying_mandarin_chinese import settings
+
+from xpinyin import Pinyin
+import httplib
+import urllib
+
+def pinyin(s):
+    p = Pinyin()
+    s2 = p.get_pinyin(unicode(s), ' ', show_tone_marks=True)
+    return s2
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world.")
+def register(request):
+    if 'q' in request.POST:
+        q = request.POST['q']
+    else:
+        q = u'你好'
+
+    print settings.engine
+    settings.initialize()
+
+    print("q = " + q.encode('utf-8'))
+
+    template = loader.get_template('vocabulary/register.html')
+    context = RequestContext(request, {
+        'q': q,
+        'p': pinyin(q).encode('utf-8'),
+        'engine': settings.engine,
+    })
+    return HttpResponse(template.render(context))
