97e774a629db74860aa36b5fd9e8a7370c7d5cf1
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-01-04 21:45:45 +0900

    Fix deploy error.
      Use python on starting instead of bash.

--- push.bat
+++ push.bat
@@ -1,1 +1,9 @@
-\bin\cf push studying-mandarin-chinese -c "bash ./run.sh"
+SET PATH=C:\bin;%PATH%
+
+cf set-env studying-mandarin-chinese JBP_LOG_LEVEL DEBUG 
+cf files studying-mandarin-chinese app/.buildpack-diagnostics/buildpack.log
+cf files studying-mandarin-chinese logs/env.log
+cf files studying-mandarin-chinese logs/staging_task.log
+
+cf push studying-mandarin-chinese -m 512M -b https://github.com/cloudfoundry/cf-buildpack-python.git -c "python server.py"
+
