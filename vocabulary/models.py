db6109b23ec80ef54c716001c7b72a2a32aebf97
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-01-06 13:57:12 +0900

    Create a model object. (Word)
    Add database connection handling.
    
    Clean up the startup script.

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from xpinyin import Pinyin
+
+def pinyin(s):
+    p = Pinyin()
+    s2 = p.get_pinyin(unicode(s), ' ', show_tone_marks=True)
+    return s2
 
 # Create your models here.
+class Word:
+    hanji = None
+    pinyin = None
+    audio = None
+
+    def __init__(self, hanji, pinyin, audio):
+        self.hanji = hanji
+        self.pinyin = pinyin
+        self.audio = audio
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -5,31 +5,50 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from studying_mandarin_chinese import settings
 
-from xpinyin import Pinyin
 import httplib
 import urllib
+from models import Word
+from models import pinyin
 
-def pinyin(s):
-    p = Pinyin()
-    s2 = p.get_pinyin(unicode(s), ' ', show_tone_marks=True)
-    return s2
+def checkRequestParameter(req, key, default):
+    val = None
+
+    if key in req.GET:
+        val = req.GET[key]
+    elif key in req.POST:
+        val = req.POST[key]
+    else:
+        val = default
+
+    return val
+
+def connectDatabase():
+    return settings.engine[0].connect()
+
+def disconnectDatabase(conn):
+    conn.close()
 
 # Create your views here.
 def register(request):
-    if 'q' in request.POST:
-        q = request.POST['q']
-    else:
-        q = u'你好'
+    q = checkRequestParameter(request, 'q', u'你好')
 
     print settings.engine
     settings.initialize()
+
+    con = connectDatabase()
+    result = con.execute("select version()")
+    for row in result:
+        version = row['version']
+    disconnectDatabase(con)
+
+    w = Word(q, pinyin(q), None)
 
     print("q = " + q.encode('utf-8'))
 
     template = loader.get_template('vocabulary/register.html')
     context = RequestContext(request, {
-        'q': q,
-        'p': pinyin(q).encode('utf-8'),
+        'w': w,
         'engine': settings.engine,
+        'version': version,
     })
     return HttpResponse(template.render(context))
