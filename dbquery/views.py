d4fc7886e20090dc44acf9841568dadf0843a2f7
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2015-01-06 16:42:14 +0900

    Add token check on executing SQL queries.

--- dbquery/views.py
+++ dbquery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from studying_mandarin_chinese import settings
+import os
 
 def checkRequestParameter(req, key, default):
     val = None
@@ -23,23 +24,32 @@ def disconnectDatabase(conn):
 
 # Create your views here.
 def dbquery(request):
-    q = checkRequestParameter(request, 'q', u'select version()')
+    token = checkRequestParameter(request, 'DBQUERY_TOKEN', None)
+    q = checkRequestParameter(request, 'q', '')
     r = None
 
     print settings.engine
     settings.initialize()
 
-    con = connectDatabase()
-    rs = con.execute(q)
-    for row in rs:
-        if r == None:
-            r = str(row) + "\n";
-        else:
-            r = r + str(row) + "\n";
-    disconnectDatabase(con)
+    print("token: " + str(os.getenv("DBQUERY_TOKEN")) + "," + str(token))
+
+    msg = ""
+    if len(q) > 0 and token == os.getenv("DBQUERY_TOKEN"):
+        con = connectDatabase()
+        rs = con.execute(q)
+        for row in rs:
+            if r == None:
+                r = str(row) + "\n";
+            else:
+                r = r + str(row) + "\n";
+        disconnectDatabase(con)
+    elif len(q) > 0:
+        msg = "token error."
 
     template = loader.get_template('dbquery/dbquery.html')
     context = RequestContext(request, {
+        'msg': msg,
+        'token': token,
         'q': q,
         'r': r,
         'engine': settings.engine,
